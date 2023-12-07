ea51c2bfd042493fbe2300e73992f2f93e7e2948
TIFFANY MAINA <tiffmaina@gmail.com>
2023-12-07 23:34:02 +0300

    search.value

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,23 +4,40 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
+  const [inputData, setInputData] = useState({
+    selectedCategory: "All",
+    searchCategory: "All", 
+  })
+  // const [selectedCategory, setSelectedCategory] = useState("All");
 
+  // state of the input value defined
+  // const [searchCategory, setSearchCategory] = useState("")
+
+  
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    if (inputData.selectedCategory === "All") return true;
+    
+    return item.category === inputData.selectedCategory;
   });
+  // console.log(itemsToDisplay); becomes the filtered array
+  console.log(inputData.selectedCategory)
 
+  const searchItems = items.filter((item)=> {
+    if (inputData.searchCategory === "") return true;
+    return item.name === inputData.searchCategory;
+  })
+
+  console.log(inputData.searchCategory); 
+  
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+        onCategoryChange={(e)=> setInputData({...inputData, selectedCategory: (e.target.value)})} 
+        onSearchChange={(e)=> setInputData({...inputData, searchCategory: (e.target.value)})} />
       <ul className="Items">
+        {/* filtered array (itemsToDisplay) is itterated over */}
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
